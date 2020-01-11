2c446df36b918bc8d7778ef160674a7823c052d6
Zaid Kokaja <zkokaja@gmail.com>
2020-01-10 19:55:54 -0500

    Initial Commit

---
+++ OpinionatedTimer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OpinionatedTimer
+//
+//  Created by zzada on 1/10/20.
+//  Copyright © 2020 zzada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
