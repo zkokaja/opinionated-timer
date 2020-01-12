718a89b56bff16571fc5b01b3ca10af32a9a8fbf
Zaid Kokaja <zkokaja@gmail.com>
2020-01-12 12:45:49 -0500

    Add images, project files, and code for version 1.0

--- OpinionatedTimer/AppDelegate.swift
+++ OpinionatedTimer/AppDelegate.swift
@@ -6,22 +6,80 @@
 //  Copyright © 2020 zzada. All rights reserved.
 //
 
+import OSLog
 import Cocoa
+import UserNotifications
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    let notificationCenter = UNUserNotificationCenter.current()
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        // Set the icon
+        let icon = NSImage(named: "tomatoIcon")
+        icon?.isTemplate = true // inverts in dark mode
+        statusItem.button?.image = icon        
+        statusItem.menu = statusMenu
+        
+        // Request user access if needed
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if error != nil {
+                os_log("Notification permission error: %s", type: .error, error.debugDescription)
+            }
+        }
+    }
+
+    @IBAction func setTimer(_ sender: NSMenuItem) {
+
+        // Determine time interval
+        var time: Double = 1
+        if sender.title == "5 Minutes" {
+            time = 5
+        }
+        else if sender.title == "25 Minutes" {
+            time = 25
+        }
+        else if sender.title == "55 Minutes" {
+            time = 55
+        }
+        
+        os_log("Setting a timer for %d minutes", type: .info, Int(time))
+      
+        // Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Time is up"
+        content.body = "Your " + sender.title + " timer is done"
+        content.sound = UNNotificationSound.default
+        
+        // Set up the time trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time * 60, repeats: false)
+        
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                    content: content, trigger: trigger)
+
+        // Schedule the request with the system.
+        notificationCenter.add(request) { (error) in
+           if error != nil {
+            os_log("Notification request error: %s", type: .error, error.debugDescription)
+           }
+        }
+    }
+
+    // Quit
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // Remove scheduled timers
+        notificationCenter.removeAllPendingNotificationRequests()
     }
 
-
 }
-
---
+++ README.md
@@ -0,0 +1,48 @@
+<h1>
+<img src="images/tomato-color.svg" alt="Tomato Icon" width="64" />
+Opinionated Timer
+</h1>
+
+I'm a timer "menulet" with a restricted set of functionality. There are other
+timers around that have fancy bells and whistles, but I'm basic. I can only set
+timers for 5, 25, or 55 minutes. This may seem limiting, but if you allow me to
+shift your perspective a bit, then you may find it rewarding.
+
+The core concept I embody is the [Pomodoro Technique]. It's simple: once you
+decide on a task to accomplish, set me for 25 or 55 minutes, then do some
+diligent work. When I ring, you take a short 5 minute break. Rinse and repeat
+until you're done with the task, taking longer breaks at some intervals. By
+committing yourself to an amount of time dedicated for productivity, you will
+be less tempted to be allow yourself to be distracted.
+
+## Usage
+
+Once I'm installed, you'll find me in your top right menu bar among other handy items:
+
+<img src="images/menu-bar.png" alt="Menu Bar Icon" height="30" />
+
+Where you can choose a time interval: 
+
+<img src="images/menu-items.png" alt="Menu Options" width="200" />
+
+When the time's up, I'll let you know:
+
+<img src="images/notification-example.png" alt="Notification Example" width="500" />
+
+
+## Installation
+
+Build with XCode then go to Product -> Archive to get a `.app` file.
+
+## Developer
+
+The code is very simply and self explanatory. The main functionality comes
+from the [User Notifications] developer library.
+
+
+## Attribution
+
+Icons made by [Freepik](https://www.flaticon.com/authors/freepik) from [www.flaticon.com](www.flaticon.com).
+
+[Pomodoro Technique]: https://en.wikipedia.org/wiki/Pomodoro_Technique
+[User Notifications]:https://developer.apple.com/documentation/usernotifications/
